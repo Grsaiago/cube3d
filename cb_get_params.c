1a4b310eb1023ec52c48b255cd7a70e48f503695
gsaiago <gsaiago@student.42.rio>
2023-06-14 18:29:56 -0300

    Fix(file validation): Conditions and logic

--- cb_get_params.c
+++ cb_get_params.c
@@ -6,11 +6,15 @@
 /*   By: gsaiago <gsaiago@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 15:31:31 by gsaiago           #+#    #+#             */
-/*   Updated: 2023/06/14 15:40:57 by gsaiago          ###   ########.fr       */
+/*   Updated: 2023/06/14 18:28:58 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./cub3d.h"
+int	cb_get_map_size(t_list *head);
+int	mat_to_rgb(char **mat, t_rgb *rgb);
+int	cb_validate_coordinate_size(char **mat, int size);
+int	cb_validate_cf_numbers(char **mat);
 
 int	mat_to_rgb(char **mat, t_rgb *rgb)
 {
@@ -56,11 +60,43 @@ int	cb_validate_cf_numbers(char **mat)
 	return (0);
 }
 
+
+int	cb_load_params(t_list *lst, t_data *data)
+{
+	while (lst)
+	{
+		if ((cb_load_no(lst, data) != 0) || (cb_load_so(lst, data) != 0)
+			|| (cb_load_we(lst, data) != 0) || (cb_load_ea(lst, data) != 0)
+			|| (cb_load_f(lst, data) != 0) || (cb_load_c(lst, data) != 0)
+			|| (cb_load_map(lst, data) != 0))
+			{
+				if (data->NO)
+					free(data->NO);
+				if (data->SO)
+					free(data->SO);
+				if (data->WE)
+					free(data->WE);
+				if (data->EA)
+					free(data->EA);
+				if (data->map)
+					ft_free_mat(data->map);
+				return (1);
+			}
+		lst = lst->next;
+	}
+	if (!data->NO || !data->SO || !data->WE || !data->EA
+			|| !data->FOK || !data->COK || !data->map)
+		return (perror("Error!\nUndefined param"), 1);
+	return (0);
+}
+
+/*
 int	cb_load_params(t_list *lst, t_data *data)
 {
 	if ((cb_load_no(lst, data) != 0) || (cb_load_so(lst, data) != 0)
 		|| (cb_load_we(lst, data) != 0) || (cb_load_ea(lst, data) != 0)
-		|| (cb_load_f(lst, data) != 0) || (cb_load_c(lst, data) != 0))
+		|| (cb_load_f(lst, data) != 0) || (cb_load_c(lst, data) != 0)
+		|| (cb_load_map(lst, data) != 0))
 		{
 			if (data->NO)
 				free(data->NO);
@@ -70,101 +106,99 @@ int	cb_load_params(t_list *lst, t_data *data)
 				free(data->WE);
 			if (data->EA)
 				free(data->EA);
-			return (1);
+			if (data->map)
+				ft_free_mat(data->map);
 		}
-	/*
-	else if (cb_get_map(lst, data) != 0)
-		return (1);
-		*/
 	return (0);
 }
+*/
 
 int	cb_load_no(t_list *head, t_data *data)
 {
 	char	**mat;
 
-	while (head)
+	if (!data->NO && !(head->content[0]  == '\0' || ft_strchr("NSWEFC", head->content[0])))
+		return (perror("Error!\nUndefined conf param"), 1);
+	if (ft_strncmp(head->content, "NO ", 3) != 0)
+		return (0);
+	if (data->NO)
 	{
-		if (ft_strncmp(head->content, "NO ", 3) == 0)
-		{
-			mat = ft_split(head->content, ' ');
-			if (cb_validate_coordinate_size(mat, 2) != 0)
-				return (ft_free_mat(mat), 1);
-			data->NO = mat[1];
-			free(mat[0]);
-			free(mat);
-			return (0);
-		}
-		head = head->next;
+		errno = EINVAL;
+		return (perror("Error!\nDouble definition of NO"), 1);
 	}
-	errno = EINVAL;
-	return (perror("Error!\nNo 'NO' parameter"), 1);
+	mat = ft_split(head->content, ' ');
+	if (cb_validate_coordinate_size(mat, 2) != 0)
+		return (ft_free_mat(mat), 1);
+	data->NO = mat[1];
+	free(mat[0]);
+	free(mat);
+	return (0);
 }
 
 int	cb_load_so(t_list *head, t_data *data)
 {
 	char	**mat;
 
-	while (head)
+	if (!data->SO && !(head->content[0]  == '\0' || ft_strchr("NSWEFC", head->content[0])))
+		return (perror("Error!\nUndefined conf param"), 1);
+	if (ft_strncmp(head->content, "SO ", 3) != 0)
+		return (0);
+	if (data->SO)
 	{
-		if (ft_strncmp(head->content, "SO ", 3) == 0)
-		{
-			mat = ft_split(head->content, ' ');
-			if (cb_validate_coordinate_size(mat, 2) != 0)
-				return (ft_free_mat(mat), 1);
-			data->SO = mat[1];
-			free(mat[0]);
-			free(mat);
-			return (0);
-		}
-		head = head->next;
+		errno = EINVAL;
+		return (perror("Error!\nDouble definition of SO"), 1);
 	}
-	errno = EINVAL;
-	return (perror("Error!\nNo 'SO' parameter"), 1);
+	mat = ft_split(head->content, ' ');
+	if (cb_validate_coordinate_size(mat, 2) != 0)
+		return (ft_free_mat(mat), 1);
+	data->SO = mat[1];
+	free(mat[0]);
+	free(mat);
+	return (0);
 }
 
 int	cb_load_we(t_list *head, t_data *data)
 {
 	char	**mat;
 
-	while (head)
+	if (!data->WE && !(head->content[0]  == '\0' || ft_strchr("NSWEFC", head->content[0])))
+		return (perror("Error!\nUndefined conf param"), 1);
+	if (ft_strncmp(head->content, "WE ", 3) != 0)
+		return (0);
+	if (data->WE)
 	{
-		if (ft_strncmp(head->content, "WE ", 3) == 0)
-		{
-			mat = ft_split(head->content, ' ');
-			if (cb_validate_coordinate_size(mat, 2) != 0)
-				return (ft_free_mat(mat), 1);
-			data->WE = mat[1];
-			free(mat[0]);
-			free(mat);
-			return (0);
-		}
-		head = head->next;
+		errno = EINVAL;
+		return (perror("Error!\nDouble definition of WE"), 1);
 	}
-	errno = EINVAL;
-	return (perror("Error!\nNo 'WE' parameter"), 1);
+	mat = ft_split(head->content, ' ');
+	if (cb_validate_coordinate_size(mat, 2) != 0)
+		return (ft_free_mat(mat), 1);
+	data->WE = mat[1];
+	free(mat[0]);
+	free(mat);
+	return (0);
 }
 
 int	cb_load_ea(t_list *head, t_data *data)
 {
 	char	**mat;
 
-	while (head)
+	if (!data->EA && !(head->content[0]  == '\0' || ft_strchr("NSWEFC", head->content[0])))
+		return (perror("Error!\nUndefined conf param"), 1);
+	if (ft_strncmp(head->content, "EA ", 3) != 0)
+		return (0);
+	if (data->EA)
 	{
-		if (ft_strncmp(head->content, "EA ", 3) == 0)
-		{
-			mat = ft_split(head->content, ' ');
-			if (cb_validate_coordinate_size(mat, 2) != 0)
-				return (ft_free_mat(mat), 1);
-			data->EA = mat[1];
-			free(mat[0]);
-			free(mat);
-			return (0);
-		}
-		head = head->next;
+		errno = EINVAL;
+		return (perror("Error!\nDouble definition of EA"), 1);
 	}
-	errno = EINVAL;
-	return (perror("Error!\nNo 'EA' parameter"), 1);
+	mat = ft_split(head->content, ' ');
+	if (cb_validate_coordinate_size(mat, 2) != 0)
+		return (ft_free_mat(mat), 1);
+	data->EA = mat[1];
+	free(mat[0]);
+	free(mat);
+	return (0);
 }
 
 int	cb_load_f(t_list *head, t_data *data)
@@ -172,25 +206,26 @@ int	cb_load_f(t_list *head, t_data *data)
 	char	**mat;
 	char	*line;
 
-	while (head)
+	if (!data->FOK && !(head->content[0]  == '\0' || ft_strchr("NSWEFC", head->content[0])))
+		return (perror("Error!\nUndefined conf param"), 1);
+	if (ft_strncmp(head->content, "F ", 2) != 0)
+		return (0);
+	if (data->FOK)
 	{
-		if (ft_strncmp(head->content, "F ", 2) == 0)
-		{
-			line = ft_strchr(head->content, ' ');
-			while (ft_isspace(*line))
-				line++;
-			mat = ft_split(line, ',');
-			if (cb_validate_coordinate_size(mat, 3)
-					|| cb_validate_cf_numbers(mat))
-				return (ft_free_mat(mat), 1);
-			mat_to_rgb(mat, &data->F);
-			ft_free_mat(mat);
-			return (0);
-		}
-		head = head->next;
+		errno = EINVAL;
+		return (perror("Error!\nDouble definition of F"), 1);
 	}
-	errno = EINVAL;
-	return (perror("Error!\nNo 'F' parameter"), 1);
+	line = ft_strchr(head->content, ' ');
+	while (ft_isspace(*line))
+		line++;
+	mat = ft_split(line, ',');
+	if (cb_validate_coordinate_size(mat, 3)
+			|| cb_validate_cf_numbers(mat))
+		return (ft_free_mat(mat), 1);
+	mat_to_rgb(mat, &data->F);
+	ft_free_mat(mat);
+	data->FOK = 1;
+	return (0);
 }
 
 int	cb_load_c(t_list *head, t_data *data)
@@ -198,23 +233,86 @@ int	cb_load_c(t_list *head, t_data *data)
 	char	**mat;
 	char	*line;
 
+	if (!data->COK && !(head->content[0]  == '\0' || ft_strchr("NSWEFC", head->content[0])))
+		return (perror("Error!\nUndefined conf param"), 1);
+	if (ft_strncmp(head->content, "C ", 2) != 0)
+		return (0);
+	if (data->COK)
+	{
+		errno = EINVAL;
+		return (perror("Error!\nDouble definition of C"), 1);
+	}
+	line = ft_strchr(head->content, ' ');
+	while (ft_isspace(*line))
+		line++;
+	mat = ft_split(line, ',');
+	if (cb_validate_coordinate_size(mat, 3)
+			|| cb_validate_cf_numbers(mat))
+		return (ft_free_mat(mat), 1);
+	mat_to_rgb(mat, &data->C);
+	ft_free_mat(mat);
+	data->COK = 1;
+	return (0);
+}
+
+int	cb_load_map(t_list *head, t_data *data)
+{
+	char	*line;
+	int		map_size;
+	int		aux;
+
+	if (!data->NO || !data->SO || !data->WE || !data->EA
+			|| !data->FOK || !data->COK || data->map)
+		return (0);
+	map_size = cb_get_map_size(head);
+	if (map_size <= 0)
+		return (1);
+	data->map = ft_calloc(map_size + 1, sizeof(char **));
+	if (!data->map)
+		return (1);
 	while (head)
 	{
-		if (ft_strncmp(head->content, "C ", 2) == 0)
-		{
-			line = ft_strchr(head->content, ' ');
-			while (ft_isspace(*line))
-				line++;
-			mat = ft_split(line, ',');
-			if (cb_validate_coordinate_size(mat, 3)
-					|| cb_validate_cf_numbers(mat))
-				return (ft_free_mat(mat), 1);
-			mat_to_rgb(mat, &data->C);
-			ft_free_mat(mat);
-			return (0);
-		}
+		line = head->content;
+		while (ft_isspace(*line))
+			line++;
+		if (*line == '1')
+			break ;
 		head = head->next;
 	}
-	errno = EINVAL;
-	return (perror("Error!\nNo 'C' parameter"), 1);
+	aux = 0;
+	while (map_size > 0)
+	{
+		data->map[aux] = ft_strdup(head->content, 0);
+		if (!data->map[aux])
+			return (1);
+		head = head->next;
+		map_size--;
+		aux++;
+	}
+	return (0);
+}
+
+int	cb_get_map_size(t_list *head)
+{
+	int		i;
+	char	*line;
+
+	while (head)
+	{
+		line = head->content;
+		while (ft_isspace(*line))
+			line++;
+		if (*line == '1')
+			break ;
+		head = head->next;
+	}
+	if (!head)
+		return (perror("Error!\nThere is no valid map"), -1);
+	i = 0;
+	while(head && head->content[0])
+	{
+		i++;
+		head = head->next;
+	}
+	return (i);
 }
--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: gsaiago <gsaiago@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:36:14 by gsaiago           #+#    #+#             */
-/*   Updated: 2023/06/14 15:31:12 by gsaiago          ###   ########.fr       */
+/*   Updated: 2023/06/14 17:28:56 by gsaiago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,17 @@ typedef struct s_rgb
 	unsigned char	b;
 } t_rgb;
 
-
 typedef struct s_data
 {
 	char	*NO;
 	char	*SO;
 	char	*WE;
 	char	*EA;
+	char	**map;
 	t_rgb	F;
+	int		FOK;
 	t_rgb	C;
+	int		COK;
 } t_data;
 
 /* info validation */
